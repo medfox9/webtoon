895c871bda8176f17aa0e0baf2176e3ba7591774
anjun <medtyche@naver.com>
2015-10-27 00:24:48 +0900

    Rejected commit: Default

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('.page').bxSlider({
+
+   });
+   $('.txt1').rotate({ angle: -10});
+   $('.txt3').rotate({ angle: 30});
+   $('.txt5').rotate({ angle: -15});
+   $('.txt6').rotate({ angle: 20});
+});
